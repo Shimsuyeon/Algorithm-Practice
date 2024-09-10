7f8ffde4386c8afa55d2f60d6acc45e4260554e0
Shim su yeon <suyeonshim@gm.gist.ac.kr>
2024-09-10 15:21:58 +0900

    240910 백준 1254 주석 수정

--- 240910_#1254_팰린드롬만들기.py
+++ 240910_#1254_팰린드롬만들기.py
@@ -3,6 +3,7 @@ import sys
 s = str(input())
 
 for i in range(len(s)):
+    # print(s, s[i:], s[i:][::-1], s[:i][::-1])
     if s[i:] == s[i:][::-1]:
         print(len(s+s[:i][::-1]))
         break
